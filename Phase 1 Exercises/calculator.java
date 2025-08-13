1f0bd14af08e9cf2c637ee8410c713f1ee43de0c
HARISREE S <shemol.suresh@gmail.com>
2025-08-13 06:47:07 +0530

    Update calculator.java
    oop calculator

--- Phase 1 Exercises/calculator.java
+++ Phase 1 Exercises/calculator.java
@@ -1,24 +1,49 @@
 //oop calculaator
 
 import java.util.Scanner;
-public class calculator
+class calculator
 {
+   double add (double x,double y)
+   {
+    return x + y;
+   }
+    double sub(double x,double y)
+   {
+    return x - y ;
+   }
+    double pro(double x,double y)
+   {
+      return x * y ;
+    }
+    double div(double x,double y)
+    {
+      if (y==0)
+      {
+        System.out.println("Cannot divide by Zero!");
+        return 0;
+      }
+      return x / y;
+    }
+ }
+class Main
+ {
   public static void main(String[] args)
   {
+    
     Scanner number = new Scanner(System.in);
+    calculator cal = new calculator();
     System.out.println("Enter first number : ");
     double x = number.nextDouble();
     System.out.println("Enter the second number : ");
     double y = number.nextDouble();
-    double add = x + y;
-    double sub = x - y ;
-    double pro = x * y ;
-    double div = x / y;
-    System.out.println("Addition : "+add);
-    System.out.println("Substration : "+sub);
-    System.out.println("Multiplication : "+pro);
-    System.out.println("Division : "+div);
+   
+    System.out.println("Addition : "+cal.add(x,y));
+    System.out.println("Substration : "+cal.sub(x,y));
+    System.out.println("Multiplication : "+cal.pro(x,y));
+    System.out.println("Division : "+cal.div(x,y));
   }
 }
+
+
 
     
